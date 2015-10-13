43ac7382a3ae0696c778fbd716c5dd5d9f8bae0f
Savinov Kirill <kirill@savinov.me>
2015-10-13 11:35:03 +0300

    initial commit

---
+++ StoreCube.php
@@ -0,0 +1,36 @@
+<?php
+namespace cubiclab\store;
+
+use Yii;
+use yii\base\BootstrapInterface;
+
+/**
+ * @version 0.0.1-prealpha
+ */
+class StoreCube extends \yii\base\Module implements BootstrapInterface
+{
+    /** @const VERSION Module version */
+    const VERSION = "0.0.1-prealpha";
+
+    public static $menu =
+        ['label' => 'Store', 'icon' => 'fa-cart', 'items' => [
+            ['label' => 'All users', 'url' => ['/admin/store']],
+        ]];
+
+    /** @inheritdoc */
+    public function init()
+    {
+        parent::init();
+        $this->registerTranslations();
+    }
+
+    public function registerTranslations()
+    {
+        if (empty(Yii::$app->i18n->translations['storecube'])) {
+            Yii::$app->i18n->translations['storecube'] = [
+                'class' => 'yii\i18n\PhpMessageSource',
+                'basePath' => __DIR__ . '/messages',
+            ];
+        }
+    }
+}
