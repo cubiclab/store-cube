e5e1c4885afa7ad0f3d35d553d1b10bc07ab134c
Savinov Kirill <kirill@savinov.me>
2015-11-18 08:40:35 +0300

    orders migration

--- migrations/m200809_000004_init_orders_table.php
+++ migrations/m200809_000004_init_orders_table.php
@@ -9,7 +9,7 @@
 use yii\db\Schema;
 use yii\db\Migration;
 
-class m200809_000003_init_dap_terms_table extends Migration
+class m200809_000004_init_orders_table extends Migration
 {
     public function safeUp()
     {
@@ -19,40 +19,35 @@ class m200809_000003_init_dap_terms_table extends Migration
         }
 
         // Delivery & Payment variants table
-        $this->createTable('{{%dap_terms}}', [
+        $this->createTable('{{%orders}}', [
             'id'            => $this->primaryKey(),
-            'type'          => $this->string(8)->notNull(),
-            'name'          => $this->string(64)->notNull(),
-            'description'   => $this->text(),
-            'price'         => $this->money(10,2),
-            'discount'      => $this->smallInteger(3),
-            'icon'          => $this->string(32),
+            'dap_id'        => $this->integer()->notNull(),
             'status'        => $this->smallInteger(1)->notNull(),
-            'order'         => $this->integer(),
+            'name'          => $this->string(64)->notNull(),
+            'address'       => $this->string(255)->notNull(),
+            'phone'         => $this->string(64)->notNull(),
+            'email'         => $this->string(128)->notNull(),
+            'comment'       => $this->string(1024)->notNull(),
+            'remark'        => $this->string(1024)->notNull(),
+            'access_token'  => $this->string(32)->notNull(),
+            'total_price'   => $this->money(10,2),
+            'ip'            => $this->string(16)->notNull(),
+            'created_at'    => $this->integer()->defaultValue(NULL),
+            'updated_at'    => $this->integer()->defaultValue(NULL),
+            'created_by'    => $this->integer()->defaultValue(NULL),
+            'updated_by'    => $this->integer()->defaultValue(NULL),
         ], $tableOptions);
 
-        //Fill Default Values
-        $this->execute($this->fillDefaultDelivery());
-        $this->execute($this->fillDefaultPayment());
-    }
+        // Foreign Keys
+        $this->addForeignKey('FK_orders_dap', '{{%orders}}', 'dap_id', '{{%dap_terms}}', 'id', 'RESTRICT', 'CASCADE');
 
-    /** @return string SQL add default delivery service */
-    private function fillDefaultDelivery()
-    {
-        return "INSERT INTO {{%dap_terms}} (id,`type`,`name`,description,price,discount,icon,status,`order`)
-                VALUES (NULL, 'delivery','Доставка курьером', 'Товар доставляется курьером и вручается в руки.', '350.00', '', '', '1', '10')";
-    }
-
-    /** @return string SQL add default delivery service */
-    private function fillDefaultPayment()
-    {
-        return "INSERT INTO {{%dap_terms}} (id,`type`,`name`,description,price,discount,icon,status,`order`)
-                VALUES (NULL, 'payment','Наличными', 'Оплата производится курьеру', '', '', '','1', '10')";
     }
 
     public function safeDown()
     {
-        $this->dropTable('{{%dap_terms}}');
+        $this->dropForeignKey('FK_orders_dap', '{{%orders}}');
+
+        $this->dropTable('{{%orders}}');
     }
 
 }
