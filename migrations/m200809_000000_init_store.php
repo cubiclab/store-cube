4b8c84c85a2cc90b81a7208323c5eeed4f91e9af
Savinov Kirill <kirill@savinov.me>
2015-11-18 11:26:51 +0300

    added rbac init

--- migrations/m200809_000000_init_store.php
+++ migrations/m200809_000000_init_store.php
@@ -9,6 +9,76 @@ class m200809_000000_init_store extends Migration
     {
         //Activate Store Cube
         $this->execute($this->activateStoreCube());
+
+        $permissions = [
+            //Categories
+            'ACPCategoriesView' => 'Can view Categories',
+            'ACPCategoriesCreate' => 'Can create Categories',
+            'ACPCategoriesUpdate' => 'Can update Categories',
+            'ACPCategoriesDelete' => 'Can delete Categories',
+            //DAP
+            'ACPDAPView' => 'Can view DAP',
+            'ACPDAPCreate' => 'Can create DAP',
+            'ACPDAPUpdate' => 'Can update DAP',
+            'ACPDAPDelete' => 'Can delete DAP',
+            //Orders
+            'ACPOrdersView' => 'Can view Orders',
+            'ACPOrdersCreate' => 'Can create Orders',
+            'ACPOrdersUpdate' => 'Can update Orders',
+            'ACPOrdersDelete' => 'Can delete Orders',
+            //Parameters
+            'ACPParametersView' => 'Can view Parameters',
+            'ACPParametersCreate' => 'Can create Parameters',
+            'ACPParametersUpdate' => 'Can update Parameters',
+            'ACPParametersDelete' => 'Can delete Parameters',
+            //Products
+            'ACPProductsView' => 'Can view Products',
+            'ACPProductsCreate' => 'Can create Products',
+            'ACPProductsUpdate' => 'Can update Products',
+            'ACPProductsDelete' => 'Can delete Products',
+        ];
+
+        foreach ($permissions as $permission => $description) {
+            $permit = Yii::$app->authManager->createPermission($permission);
+            $permit->description = $description;
+            Yii::$app->authManager->add($permit);
+        }
+
+        //set permissions to roles
+        $roles_permissions = [
+            'RootAdmin' => [
+                'ACPParametersCreate',
+                'ACPParametersUpdate',
+                'ACPParametersDelete',
+            ],
+            'Admin' => [
+                'ACPCategoriesCreate',
+                'ACPCategoriesUpdate',
+                'ACPCategoriesDelete',
+                'ACPDAPCreate',
+                'ACPDAPUpdate',
+                'ACPDAPDelete',
+                'ACPOrdersCreate',
+                'ACPOrdersUpdate',
+                'ACPOrdersDelete',
+                'ACPProductsCreate',
+                'ACPProductsUpdate',
+                'ACPProductsDelete',
+            ],
+            'Moderator' => [
+                'ACPCategoriesView',
+                'ACPDAPView',
+                'ACPOrdersView',
+                'ACPOrdersUpdate',
+                'ACPParametersView',
+                'ACPProductsView',
+            ],
+        ];
+        foreach ($roles_permissions as $role => $permissions) {
+            foreach ($permissions as $permit) {
+                Yii::$app->authManager->addChild(Yii::$app->authManager->getRole($role), Yii::$app->authManager->getPermission($permit));
+            }
+        }
     }
 
     /** @return string SQL to activate store cube */
